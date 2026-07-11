e5d8358a6fe6ee99efe4cd0d119739cb437dc940
Riyan Hidayat <riyanhidayat.dev@gmail.com>
2026-07-11 12:45:05 +0700

    Ubah import URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,9 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
-use Sabberworm\CSS\Value\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
